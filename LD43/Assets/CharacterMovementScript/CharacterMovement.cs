dd8641d8bf80bd1c8c61565bc5fd9b98db1310be
HolmErik <erikholm@live.com>
2018-12-01 20:19:04 +0100

    Hopp fix

--- LD43/Assets/CharacterMovementScript/CharacterMovement.cs
+++ LD43/Assets/CharacterMovementScript/CharacterMovement.cs
@@ -5,24 +5,18 @@ using UnityEngine.Networking;
 
 public class CharacterMovement : NetworkBehaviour
 {
-    [SerializeField]
-    private float speed = 5;
-    [SerializeField]
-    private float airStrafeSpeed = 1.3f;
-    [SerializeField]
-    private float sprintMultiplier = 1.3f;
-    [SerializeField]
-    private float g = -9.82f;
-    [SerializeField]
-    private float jumpHeight = 10;
+
+    private float g = -15f;
     private CharacterController cc;
 
     private Vector3 velocity = Vector3.zero;
+    private Player player;
 
 
     private void Start()
     {
         cc = GetComponent<CharacterController>();
+        player = gameObject.GetComponent<Player>();
     }
 
 
@@ -34,51 +28,33 @@ public class CharacterMovement : NetworkBehaviour
         if (!cc.isGrounded)
         {
             ApplyGravity();
-            GetAirMovement();
         }
         else
         {
-            GetGroundMovement();
+            
             GetJump();
         }
+        GetMovement();
         ApplyMovement();
     }
 
 
-    void GetGroundMovement()
+    void GetMovement()
     {
         float sprint = 1.0f;
-        if (Input.GetKey(KeyCode.LeftShift)) sprint = sprintMultiplier;
+        if (Input.GetKey(KeyCode.LeftShift)) sprint = player.sprintMultiplier;
         Vector2 move;
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
-        velocity.x = move.normalized.x * speed * sprint;
-        velocity.z = move.normalized.y * speed * sprint;
-    }
-
-    void GetAirMovement()
-    {
-        Vector2 VelocityXZ = new Vector2(velocity.x, velocity.z);
-        Vector2 airStrafeVelocity = Vector2.zero;
-
-        if (velocity.x >= 0 && Input.GetAxis("Horizontal") < 0 || velocity.x <= 0 && Input.GetAxis("Horizontal") > 0 || velocity.magnitude < speed)
-        {
-            airStrafeVelocity.x = Input.GetAxis("Horizontal");
-        }
-        if (velocity.z >= 0 && Input.GetAxis("Vertical") < 0 || velocity.z <= 0 && Input.GetAxis("Vertical") > 0 || velocity.magnitude < speed)
-        {
-            airStrafeVelocity.y = Input.GetAxis("Vertical");
-        }
-        airStrafeVelocity = airStrafeVelocity.normalized;
-
-        velocity = new Vector3(velocity.x + airStrafeVelocity.x * airStrafeSpeed, velocity.y, velocity.z + airStrafeVelocity.y * airStrafeSpeed);
+        velocity.x = move.normalized.x * player.speed * sprint;
+        velocity.z = move.normalized.y * player.speed * sprint;
     }
 
 
     void GetJump()
     {
         if (Input.GetButtonDown("Jump"))
-            velocity.y = jumpHeight;
+            velocity.y = player.jumpHeight;
     }
 
 
