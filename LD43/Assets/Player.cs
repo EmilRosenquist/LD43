69d36407f86d54f9b2837e633d25e407a63a8193
Sooolan <emil.rosenquist@gmail.com>
2018-12-01 07:03:25 +0100

    Fixed Network Camera

---
+++ LD43/Assets/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour{
+    public GameObject playerCamera;
+    void Start(){
+        if (!isLocalPlayer){
+            return;
+        }
+        Instantiate(playerCamera, transform);
+    }
+}
