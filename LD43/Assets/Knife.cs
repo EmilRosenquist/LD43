92c2231a3a4143e1da4ed74e21e9e5b16cef24f1
Amanda <amandabroman98@gmail.com>
2018-12-01 21:38:38 +0100

    knife

---
+++ LD43/Assets/Knife.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : Wepond {
+
+    public float targetDistance;
+    public float maxDistance;
+
+    public override void Attack(Player player, Vector3 spawnPoint, Vector3 direction)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(player.GetComponentInChildren<Camera>().transform.position, player.GetComponentInChildren<Camera>().transform.forward, out hit))
+        {
+
+            targetDistance = hit.distance;
+
+            if (targetDistance < maxDistance)
+            {
+                GameObject hitGameObject = hit.transform.gameObject;
+
+                if (hitGameObject.GetComponent<Player>() != null)
+                {
+                    Player hitplayer = hitGameObject.GetComponent<Player>();
+
+                    if (hitplayer != player)
+                    {
+                        hitplayer.TakeDamage(10);
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    public override int CheckMagasine()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override int CheckTotalBullets()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool ReloadAmmo()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
